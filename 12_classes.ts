21d6ead312045802c500f27c77c8636b81fb2399
Leonhard <leonhard.radonic@gmail.com>
2017-09-11 20:30:20 +0200

    Classes

---
+++ 12_classes.ts
@@ -0,0 +1,30 @@
+
+class Person_12 {
+    constructor (name) {
+        this.name = name;
+        console.log(`${this.name} Constructor`);
+    }
+    static staticMethod() {
+        console.log("Static method");
+    }
+    greet(){
+        console.log(`Hello ${this.name}`);
+    }
+};
+
+let p_12 = new Person_12("Leonhard");
+Person_12.staticMethod();
+p_12.greet();
+
+class Employee_12 extends Person_12 {
+    constructor(name) {
+        super(name);
+    }
+    greet() {
+        console.log("Greet employee " + this.name);
+    }
+}
+
+let e_12 = new Employee_12("Employee Leo");
+Employee_12.staticMethod();
+e_12.greet();
